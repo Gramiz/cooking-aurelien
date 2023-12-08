930ae801cdde5efe374aa2f6e2bd79f0e2269cbb
Gramiz <94576530+Gramiz@users.noreply.github.com>
2023-12-08 16:10:18 +0100

    Mise en place des packet via nodejs. > > Co-authored-by: NAME <manon.le-bouard@etudiant.univ-reims.fr>

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 import "./style.css"
 
 import OBSWebSocket from "obs-websocket-js"
@@ -8,3 +9,47 @@ async function main() {
     console.log("connected")
 }
 main()
+*/
+const express = require("express");
+const app = express();
+const OBSWebSocket = require("obs-websocket-js");
+
+const obs = new OBSWebSocket();
+
+obs
+  .connect({ address: "localhost:4444", password: "obscontroll" })
+  .then(() => {
+    console.log("Connecté à OBS");
+
+    // Exemple de changement de scène
+    obs
+      .send("SetCurrentScene", { "scene-name": "Nom_de_votre_scène" })
+      .then(() => {
+        console.log("Scène changée avec succès");
+      })
+      .catch((err) => {
+        console.error("Erreur lors du changement de scène :", err);
+      });
+  })
+  .catch((err) => {
+    console.error("Erreur de connexion à OBS :", err);
+  });
+
+// Code pour se connecter à OBS et changer les scènes...
+
+// Exemple de route pour changer la scène en utilisant un endpoint HTTP
+app.get("/changeScene/:sceneName", async (req, res) => {
+  const sceneName = req.params.sceneName;
+  try {
+    await obs.send("SetCurrentScene", { "scene-name": sceneName });
+    res.send(`Scène changée en ${sceneName}`);
+  } catch (error) {
+    res.status(500).send("Erreur lors du changement de scène");
+  }
+});
+
+// Démarrer le serveur
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Serveur lancé sur le port ${PORT}`);
+});
